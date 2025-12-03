b1fd8a3556952304e0538a20743ee4c79765a817
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-12-03 12:32:47 +0100

    update settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,7 +1,6 @@
 export const settings = {
     defaultPlayerName1: 'John',
-    playGameFormId: 'play-game',
-    // Attention il y en a 2. 👌
+    playGameFormId: 'play-game', // Attention il y en a 2. 👌
     players_container_selector: '.player-container__player',
     logContainerSelector: '.log',
     controlDivSelector: '.controls',
@@ -12,26 +11,23 @@ export const settings = {
     playerNameId: 'player-name',
     healthMaxValue: 100,
     givUpName: 'give-up',
+    labelSelector: 'label',
+    progressSelector: 'progress',
     play_again_message: 'Rejouer',
     defaultPlayerName2: 'Monster',
     lost_message: (name) => `<p class="log__item">${name} a perdu !</p>`,
     remaining_life_message: (value) => `${value}% de vie restante`,
     actions: {
         attack: {
-            max_impact: -7,
-            message(player, damage) {
+            max_impact: -7, message(player, damage) {
                 return `<li class="log__item">${player.name} a subi une attaque simple de ${Math.abs(damage)}. Il en est à ${player.health}.</li>`;
             }
-        },
-        special_attack: {
-            max_impact: -14,
-            message(player, damage) {
+        }, special_attack: {
+            max_impact: -14, message(player, damage) {
                 return `<li class="log__item">${player.name} a subi une attaque spéciale de ${Math.abs(damage)}. Il en est à ${player.health}.</li>`;
             }
-        },
-        heal: {
-            max_impact: 7,
-            message(player, damage) {
+        }, heal: {
+            max_impact: 7, message(player, damage) {
                 return `<li class="log__item">${player.name} a été soigné de de ${Math.abs(damage)}. Il en est à ${player.health}.</li>`;
             }
         }
